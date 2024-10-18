38af3a8a340100e29f3ebfd30a9422f1ae617f89
Erike Nascimento <101950160+erikenascimento@users.noreply.github.com>
2024-10-18 00:27:30 -0300

    Modificando a URL para uma especificada

--- projeto_todo/todo/urls.py
+++ projeto_todo/todo/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import atividade_sucesso
 
 urlpatterns = [
-    path('sucesso/', atividade_sucesso, name='atividade_sucesso'),
+    path('erike-nascimento/', atividade_sucesso, name='atividade_sucesso'),
 ]
